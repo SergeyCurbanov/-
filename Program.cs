74d1cd85145ecb968adc6896b62a184478d12aae
SergeyCurbanov <81747898+SergeyCurbanov@users.noreply.github.com>
2021-05-26 14:25:36 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace WindowsFormsApp4
+namespace ConsoleApp4
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            int N = 11;
+            int[,] massive = new int[N, N];
+            for (int i = 0; i < N; i++)       
+                for (int j = 0; j < N; j++)
+                {
+                    if (i <= (N / 2))
+                        if (j <= (N / 2))
+                        {
+                            if (i == j)
+                            {
+                                massive[i, j] = 1;
+                                continue;
+                            }
+                            if (i <= j)
+                                if (j > 0)
+                                    massive[i, j] = massive[i, j - 1] + 1;
+                            if (i > j)
+                                if (i > 0)
+                                    massive[i, j] = massive[i - 1, j] + 1;
+                        }
+                    if (j > (N / 2))
+                        massive[i, j] = massive[i, (N - 1) - j];
+                    if (i > (N / 2))
+                        massive[i, j] = massive[(N - 1) - i, j];
+                }
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                    Console.Write(" " + massive[i, j]);
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
         }
     }
 }
+        
